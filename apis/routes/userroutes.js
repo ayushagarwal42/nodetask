13b0ec1f6e2aefefa98747a0735c5d76c71ea807
Ayush Agarwal <ayushagarwalds12@gmail.com>
2024-09-30 12:29:27 +0530

    changes

--- apis/routes/userroutes.js
+++ apis/routes/userroutes.js
@@ -2,8 +2,7 @@ const express= require('express');
 const router=express.Router();
 const {verifyToken, verifyAdminToken}=require("../middlewares/middleware");
 const usercontroller=require("../controller/usercontroller");
-// const ordercontroller=require("../controller/ordercontroller");
-// const productcontroller=require("../controller/productcontroller");
+
 
 
 router.post("/signup",usercontroller.signup);
@@ -12,11 +11,7 @@ router.post("/approveUser",verifyAdminToken,usercontroller.approveUser);
 router.get("/getPendingUsers",verifyAdminToken,usercontroller.getPendingUsers);
 router.get("/getUserList",verifyAdminToken,usercontroller.getUserList);
 
-// router.post("/createorder",ordercontroller.createorder);
-// router.get("/getOrdersWithDetails",ordercontroller.getOrdersWithDetails);
 
-
-// router.post("/createproduct",productcontroller.createproduct);
 
 
 module.exports=router;
