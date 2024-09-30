66e2d2da2f2e946cc3f08f3ce4b323f726e7263e
Ayush Agarwal <ayushagarwalds12@gmail.com>
2024-09-30 11:33:44 +0530

    updated readme file

--- apis/routes/productroutes.js
+++ apis/routes/productroutes.js
@@ -1,3 +1,5 @@
 const express = require("express");
 const router = express.Router();
 const productcontroller = require("../controller/productcontroller");
+
+router.post("/addproduct", productcontroller.addProduct);
--- apis/routes/warehouseroutes.js
+++ apis/routes/warehouseroutes.js
@@ -1,3 +1,5 @@
 const express = require("express");
 const router = express.Router();
 const warehousecontroller = require("../controller/warehousecontroller");
+
+router.post("/addwarehouse", warehousecontroller.addWarehouse);
